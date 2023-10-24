0c7ea9c01d1858ef143e776cf8b909fe81f3d7b2
Sinx <gabriel.jbrito2@gmail.com>
2023-10-24 16:58:00 -0300

    Classe de Serviços onde será feito as operações para API, como os construtores.

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 class JournalService{
+  //Se conectando na API
   static const String url='http://192.168.0.37:3000/';
   static const String resource = 'learnhttp/';
 
@@ -7,6 +8,7 @@ class JournalService{
     return '$url$resource';
   }
 
+  //Criando um construtor de registro (POST)
   register(String content){
   http.post(Uri.parse(getUrl()), body: {
     'content': content
