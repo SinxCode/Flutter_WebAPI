9b811a8fab349e9b5c7508df58259ab53e3bd60e
Sinx <gabriel.jbrito2@gmail.com>
2023-10-30 13:28:41 -0300

    Salvando informação de autenticação localmente

--- android/app/src/main/kotlin/br/com/alura/flutter_webapi_first_course/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/alura/flutter_webapi_first_course/MainActivity.kt
@@ -3,4 +3,8 @@ package br.com.alura.flutter_webapi_first_course
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine) 
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter_webapi_first_course/models/journal.dart';
 import 'package:flutter_webapi_first_course/screens/home_screen/add_journal_screen/add_journal_screen.dart';
 import 'package:flutter_webapi_first_course/screens/home_screen/login_Screen/login_screen.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 
   JournalService service = JournalService();
+  SharedPreferences.setMockInitialValues({});
   //service.register(Journal.empty());
   //service.getAll();
 }
